dc46ea8a8fcb2d90455b9f2e0c2a2a39677da11a
ALighator <brunoegsilva@hotmail.com>
2024-03-28 09:41:06 -0300

    estrutura inicial do projeto

---
+++ src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    private String nome;
+    private int codigo;
+    private long cpf;
+    private Endereco endereco;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
